478686694375e5d05f7eb0f672527120896c0387
sergiioo101 <smartros@myuax.com>
2024-03-13 16:55:31 +0100

    10

--- src/main/java/AdministracionRecursos/Comida.java
+++
@@ -1,7 +0,0 @@
-package AdministracionRecursos;
-
-public class Comida extends Recursos {
-    public Comida(String name, int quantity) {
-        super(name, quantity);
-    }
-}
--- src/main/java/AdministracionRecursos/Equipamiento.java
+++
@@ -1,4 +0,0 @@
-package AdministracionRecursos;
-
-public class Equipamiento {
-}
