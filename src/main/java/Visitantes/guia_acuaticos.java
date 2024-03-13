d253a4af0332c76f6e94be49365cb376c2bba181
sergiioo101 <smartros@myuax.com>
2024-03-13 18:45:42 +0100

    28

--- src/main/java/Visitantes/guia_acuaticos.java
+++ src/main/java/Visitantes/guia_acuaticos.java
@@ -1,7 +1,6 @@
 package Visitantes;
-
 import Gestion_habitats.habitats;
-
+// En la clase guia_acuaticos
 public class guia_acuaticos extends guia {
     @Override
     public void ofrecerGuia() {
@@ -10,6 +9,7 @@ public class guia_acuaticos extends guia {
 
     @Override
     public void ofrecerGuia(habitats habitat) {
-
+        super.ofrecerGuia(habitat);
+        System.out.println("Te damos la bienvenida al recorrido de animales acuáticos.");
     }
 }
