9ef39d0874657a910b0300d8e6a6979eb1ce723a
sergiioo101 <smartros@myuax.com>
2024-03-13 17:24:11 +0100

    13

--- src/main/java/Visitantes/GuiaMamiferos.java
+++ src/main/java/Visitantes/GuiaMamiferos.java
@@ -3,6 +3,6 @@ package Visitantes;
 public class GuiaMamiferos extends Guia {
     @Override
     public void ofrecerGuia() {
-        System.out.println("Bienvenido al tour de mamíferos. Aquí podrás ver diferentes tipos de mamíferos y aprender sobre ellos.");
+        System.out.println("Te damos la bienvenida al recorrido de mamíferos.");
     }
 }
--- src/main/java/Visitantes/guia_acuaticos.java
+++ src/main/java/Visitantes/guia_acuaticos.java
@@ -1,6 +1,6 @@
 package Visitantes;
 
-public class GuiaAcuaticos extends Guia {
+public class guia_acuaticos extends Guia {
     @Override
     public void ofrecerGuia() {
         System.out.println("Bienvenido al tour de los animales acuáticos. Aquí podrás ver diferentes tipos de animales acuáticos y aprender sobre ellos.");
